b3a905bfbc6bf80cd263eac4dc46e80bd31519dd
Carlos Becker <68871958+carlosbecker2077@users.noreply.github.com>
2022-01-31 15:40:24 -0300

    Update livraria_fisico.sql

--- livraria_fisico.sql
+++ livraria_fisico.sql
@@ -1,33 +1,32 @@
 create table Remissiva (
 	ID int identity(1,1) primary key,
-	nome_remissiva nvarchar(100)
-
+	nome_remissiva nvarchar(100) not null
 );
 
 create table Genero (
 	id_genero int identity(1,1) primary key,
-	nome_genero varchar(100)
+	nome_genero varchar(100) not null
 );
 
 create table UF (
 	UF char(2) primary key,
-	nome_uf varchar(100)
+	nome_uf varchar(100) not null
 );
 
 create table Pais (
 	ID_pais int identity(1,1) primary key,
-	nome_pais varchar(100)
+	nome_pais varchar(100) not null
 );
 
 create table Cidade (
 	ID_cidade int identity(1,1) primary key,
-	nome_cidade varchar(100),
+	nome_cidade varchar(100) not null, 
 	cod_ibge varchar(30)
 );
 
 create table Editora (
 	ID_editora int identity(1,1) primary key,
-	nome varchar(100),
+	nome varchar(100) not null,
 	fone1 varchar(30),
 	fone2 varchar(30),
 	email varchar(100),
@@ -42,19 +41,19 @@ create table Editora (
 
 create table Edicao (
 	ID_edicao int identity(1,1) primary key,
-	num_edicao int,
-	tit_edicao nvarchar(100),
+	num_edicao int not null,
+	tit_edicao nvarchar(100) not null,
 	dt_public date,
-	num_paginas integer,
+	num_paginas integer not null,
 	local_public varchar(100),
 	idioma varchar(30),	
-	qtde_estoque decimal(18,3),
-	preco decimal(18,3),
+	qtde_estoque decimal(18,3) not null,
+	preco decimal(18,3) not null,
 );
 
 create table Autor (
 	ID_autor int identity(1,1) primary key,
-	nome nvarchar(100),
+	nome nvarchar(100) not null,
 	dt_nascimento date,
 	obs_bio varchar(500),
 	id_pais int,
@@ -66,17 +65,17 @@ create table Livro (
 	GTIN_EAN varchar(30) not null,
 	ISBN varchar(30),
 	titulo_original nvarchar(100),
-	ID_edicao int
+	ID_edicao int not null
 );
 
 create table Autor_Livro (
 	ID int identity(1,1) primary key,
-	ID_livro int,
-	ID_autor int
+	ID_livro int not null,
+	ID_autor int not null
 );
 
 create table Genero_Livro (
 	ID int identity(1,1) primary key,
-	ID_livro int,
-	ID_genero int
+	ID_livro int not null,
+	ID_genero int not null
 );
