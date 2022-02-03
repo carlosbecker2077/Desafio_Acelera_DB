3fc8dbd50d68b1b9e4cc181a6027c90839df18ab
Carlos Becker <68871958+carlosbecker2077@users.noreply.github.com>
2022-02-02 21:33:27 -0300

    inserts

---
+++ insert_generos.sql
@@ -0,0 +1,69 @@
+insert into genero (nome_genero)
+values ('Literatura inglesa');
+
+insert into genero (nome_genero)
+values ('Ficção científica');
+
+insert into genero (nome_genero)
+values ('Romance');
+
+insert into genero (nome_genero)
+values ('Poesia');
+
+insert into genero (nome_genero)
+values ('Literatura portuguesa');
+
+insert into genero (nome_genero)
+values ('Infanto-juvenil');
+
+insert into genero (nome_genero)
+values ('Política');
+
+insert into genero (nome_genero)
+values ('História');
+
+insert into genero (nome_genero)
+values ('Fantasia');
+
+insert into genero (nome_genero)
+values ('Ação');
+
+insert into genero (nome_genero)
+values ('Aventura');
+
+insert into genero (nome_genero)
+values ('Horror');
+
+insert into genero (nome_genero)
+values ('Thriller/Suspense');
+
+insert into genero (nome_genero)
+values ('Biografia');
+
+insert into genero (nome_genero)
+values ('Gastronomia');
+
+insert into genero (nome_genero)
+values ('Arte/Fotografia');
+
+insert into genero (nome_genero)
+values ('Auto Ajuda');
+
+insert into genero (nome_genero)
+values ('Religião');
+
+insert into genero (nome_genero)
+values ('Tecnologia/Ciência');
+
+insert into genero (nome_genero)
+values ('Infantil');
+
+insert into genero (nome_genero)
+values ('Mangá');
+
+insert into genero (nome_genero)
+values ('Gibis');
+
+select * from Genero
+insert into genero (nome_genero)
+values ('Literatura russa');
--- livraria_fisico.sql
+++ livraria_fisico.sql
@@ -1,10 +1,11 @@
 create table Remissiva (
 	ID int identity(1,1) primary key,
-	nome_remissiva nvarchar(100) not null
+	nome_remissiva nvarchar(100) not null,
+	id_autor int not null
 );
 
 create table Genero (
-	id_genero int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	nome_genero varchar(100) not null
 );
 
@@ -14,18 +15,18 @@ create table UF (
 );
 
 create table Pais (
-	ID_pais int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	nome_pais varchar(100) not null
 );
 
 create table Cidade (
-	ID_cidade int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	nome_cidade varchar(100) not null, 
 	cod_ibge varchar(30)
 );
 
 create table Editora (
-	ID_editora int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	nome varchar(100) not null,
 	fone1 varchar(30),
 	fone2 varchar(30),
@@ -39,8 +40,9 @@ create table Editora (
 	ID_Cidade int
 );
 
+
 create table Edicao (
-	ID_edicao int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	num_edicao int not null,
 	tit_edicao nvarchar(100) not null,
 	dt_public date,
@@ -49,23 +51,24 @@ create table Edicao (
 	idioma varchar(30),	
 	qtde_estoque decimal(18,3) not null,
 	preco decimal(18,3) not null,
+	ID_livro int not null,
+	ID_editora int not null
 );
 
 create table Autor (
-	ID_autor int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	nome nvarchar(100) not null,
 	dt_nascimento date,
 	obs_bio varchar(500),
-	id_pais int,
-	id_remissiva int
+	id_pais int
 );
 
 create table Livro (
-	ID_livro int identity(1,1) primary key,
+	ID int identity(1,1) primary key,
 	GTIN_EAN varchar(30) not null,
 	ISBN varchar(30),
 	titulo_original nvarchar(100),
-	ID_edicao int not null
+	
 );
 
 create table Autor_Livro (
@@ -79,3 +82,36 @@ create table Genero_Livro (
 	ID_livro int not null,
 	ID_genero int not null
 );
+
+ALTER TABLE Genero_Livro
+ADD FOREIGN KEY (ID_livro) REFERENCES Livro(ID);
+
+ALTER TABLE Genero_Livro
+ADD FOREIGN KEY (ID_genero) REFERENCES Genero(ID);
+
+ALTER TABLE Remissiva
+ADD FOREIGN KEY (ID_autor) REFERENCES Autor(ID);
+
+ALTER TABLE Autor_Livro
+ADD FOREIGN KEY (ID_livro) REFERENCES Livro(ID);
+
+ALTER TABLE Autor_Livro
+ADD FOREIGN KEY (ID_autor) REFERENCES Autor(ID);
+
+ALTER TABLE Autor
+ADD FOREIGN KEY (ID_pais) REFERENCES Pais(ID);
+
+ALTER TABLE Editora
+ADD FOREIGN KEY (ID_pais) REFERENCES Pais(ID);
+
+ALTER TABLE Editora
+ADD FOREIGN KEY (UF) REFERENCES UF(UF);
+
+ALTER TABLE Editora
+ADD FOREIGN KEY (ID_cidade) REFERENCES Cidade(ID);
+
+ALTER TABLE Edicao
+ADD FOREIGN KEY (ID_livro) REFERENCES Livro(ID);
+
+ALTER TABLE Edicao
+ADD FOREIGN KEY (ID_editora) REFERENCES Editora(ID);
